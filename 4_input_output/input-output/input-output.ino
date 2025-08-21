7a3e80bed12f743f8439131d93be5cc1167993d3
maftuh.sh <maftuhsirojhamdaniii@gmail.com>
2025-08-22 03:05:12 +0700

    Update

---
+++ 4_input_output/input-output/input-output.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+
+
+}
+
+void loop() {
+
+
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  Serial.println(digitalRead(13));
+  delay(1000);
+  digitalWrite(13, LOW);
+  Serial.println(digitalRead(13));
+  delay(1000);
+
+}
